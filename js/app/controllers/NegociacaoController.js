8f302f6946108400f2745324c1cc26755173f84d
Lucas Adams <adams.lucass@gmail.com>
2017-05-04 23:44:36 -0300

    primeiro modulo

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,28 +1,31 @@
 class NegociacaoController {
     
     constructor() {
-        
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
-        
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
-        
+
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagemView.update(this._mensagem);
-        
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+
+        this._mensagem.texto = 'Negociaçoes apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
     }
     
     adiciona(event) {
-        
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
-        
+
         this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
         
@@ -30,7 +33,6 @@ class NegociacaoController {
     }
     
     _criaNegociacao() {
-        
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
@@ -38,7 +40,6 @@ class NegociacaoController {
     }
     
     _limpaFormulario() {
-     
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,17 +1,19 @@
 class ListaNegociacoes {
     
-    constructor() {
-        
+    constructor(armadilha) {
         this._negociacoes = [];
+        this._armadilha = armadilha;
     }
     
     adiciona(negociacao) {
-        
         this._negociacoes.push(negociacao);
     }
     
     get negociacoes() {
-        
         return [].concat(this._negociacoes);
+    }
+
+    esvazia() {
+        this._negociacoes = [];
     }
 }
